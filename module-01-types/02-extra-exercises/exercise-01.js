1c62e9a72e6cd2a232501cd1044fb02ebcbd2b48
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-04-16 18:27:07 -0300

    feat: implement exercise 01 and add initial structure for exercise 02

---
+++ module-01-types/02-extra-exercises/exercise-01.js
@@ -0,0 +1,63 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+function tipoReal(valor) {
+    if (valor === null) {
+        return "null"
+    }
+
+    if (Array.isArray(valor)) {
+        return "array"
+    }
+
+    if (Number.isNaN(valor)) {
+        return "nan"
+    }
+
+    return typeof valor
+}
+
+printTitulo("NÍVEL 1")
+console.log(tipoReal("caio"))
+console.log(tipoReal(10))
+console.log(tipoReal(true))
+console.log(tipoReal(null))
+console.log(tipoReal([1, 2, 3]))
+console.log(tipoReal(undefined))
+console.log(tipoReal(NaN))
+
+// Nível 2 (Em progresso...)
+
+function tipoReal(valor) {
+    if (valor === null) {
+        return "null"
+    }
+
+    if (Array.isArray(valor)) {
+        return "array"
+    }
+
+    if (Number.isNaN(valor)) {
+        return "nan"
+    }
+
+    return typeof valor
+}
+
+printTitulo("NÍVEL 2")
+console.log(tipoReal("abc"))
+console.log(tipoReal(10))
+console.log(tipoReal(true))
+console.log(tipoReal(undefined))
+console.log(tipoReal(null))
+console.log(tipoReal([1, 2, 3]))
+console.log(tipoReal({nome: "caio"}))
+console.log(tipoReal(NaN))
+console.log(tipoReal(() => {}))
+console.log(tipoReal(new Date()))
+console.log(tipoReal(/abc/))
