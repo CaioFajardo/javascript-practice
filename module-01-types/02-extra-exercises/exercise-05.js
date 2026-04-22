4fc8f892931dc6b766da160cc9d8b9204bf52a7e
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-04-22 20:46:58 -0300

    refactor: added new exercises to exercise-05 (4-6)

--- module-01-types/02-extra-exercises/exercise-05.js
+++ module-01-types/02-extra-exercises/exercise-05.js
@@ -6,7 +6,7 @@ function printTitulo(titulo) {
 
 // Nível 1
 
-function mesmoTipo(array) {
+function mesmoTipoNivel1(array) {
     if (!Array.isArray(array)) {
         return "Erro: Valor inválido!"
     }
@@ -28,12 +28,12 @@ function mesmoTipo(array) {
 
 printTitulo("NÍVEL 1")
 
-console.log(mesmoTipo([1, 2, 3]))
-console.log(mesmoTipo([1, "2", 3]))
+console.log(mesmoTipoNivel1([1, 2, 3]))
+console.log(mesmoTipoNivel1([1, "2", 3]))
 
 // Nível 2
 
-function mesmoTipo(array) {
+function mesmoTipoNivel2(array) {
     if (!Array.isArray(array)) {
         return "Erro: Valor inválido!"
     }
@@ -55,21 +55,21 @@ function mesmoTipo(array) {
 
 printTitulo("NÍVEL 2")
 
-console.log(mesmoTipo([1, 2, 3]))
-console.log(mesmoTipo([1, "2", 3]))
-console.log(mesmoTipo([1]))
-console.log(mesmoTipo(["a"]))
+console.log(mesmoTipoNivel2([1, 2, 3]))
+console.log(mesmoTipoNivel2([1, "2", 3]))
+console.log(mesmoTipoNivel2([1]))
+console.log(mesmoTipoNivel2(["a"]))
 
-console.log(mesmoTipo(null))
-console.log(mesmoTipo(NaN))
-console.log(mesmoTipo([]))
+console.log(mesmoTipoNivel2(null))
+console.log(mesmoTipoNivel2(NaN))
+console.log(mesmoTipoNivel2([]))
 
-console.log(mesmoTipo([null, null]))
-console.log(mesmoTipo([null, {}]))
+console.log(mesmoTipoNivel2([null, null]))
+console.log(mesmoTipoNivel2([null, {}]))
 
 // Nível 3
 
-function getTipo(valor) {
+function getTipoNivel3(valor) {
     if (valor === null) {
         return "null"
     }
@@ -77,7 +77,7 @@ function getTipo(valor) {
     return typeof valor
 }
 
-function mesmoTipo(array) {
+function mesmoTipoNivel3(array) {
     if (!Array.isArray(array)) {
         return "Erro: Valor inválido!"
     }
@@ -86,10 +86,10 @@ function mesmoTipo(array) {
         return "Erro: Valor inválido!"
     }
 
-    let posUm = getTipo(array[0])
+    let posUm = getTipoNivel3(array[0])
 
     for (let i = 1; i < array.length; i++) {
-        if (getTipo(array[i]) !== posUm) {
+        if (getTipoNivel3(array[i]) !== posUm) {
             return false
         }
     }
@@ -99,14 +99,168 @@ function mesmoTipo(array) {
 
 printTitulo("NÍVEL 3")
 
-console.log(mesmoTipo([1, 2, 3]))
-console.log(mesmoTipo([1, "2", 3]))
-console.log(mesmoTipo([1]))
-console.log(mesmoTipo(["a"]))
+console.log(mesmoTipoNivel3([1, 2, 3]))
+console.log(mesmoTipoNivel3([1, "2", 3]))
+console.log(mesmoTipoNivel3([1]))
+console.log(mesmoTipoNivel3(["a"]))
 
-console.log(mesmoTipo(null))
-console.log(mesmoTipo(NaN))
-console.log(mesmoTipo([]))
+console.log(mesmoTipoNivel3(null))
+console.log(mesmoTipoNivel3(NaN))
+console.log(mesmoTipoNivel3([]))
 
-console.log(mesmoTipo([null, null]))
-console.log(mesmoTipo([null, {}]))
+console.log(mesmoTipoNivel3([null, null]))
+console.log(mesmoTipoNivel3([null, {}]))
+
+// Nível 4
+
+function getTipoNivel4(valor) {
+    if (valor === null) {
+        return "null"
+    }
+
+    if (Number.isNaN(valor)) {
+        return "nan"
+    }
+
+    return typeof valor
+}
+
+function mesmoTipoNivel4(array) {
+    if (!Array.isArray(array)) {
+        return "Erro: Valor inválido!"
+    }
+
+    if (array.length === 0) {
+        return "Erro: Valor inválido!"
+    }
+
+    let posUm = getTipoNivel4(array[0])
+
+    for (let i = 1; i < array.length; i++) {
+        if (getTipoNivel4(array[i]) !== posUm) {
+            return false
+        }
+    }
+
+    return true
+}
+
+printTitulo("NÍVEL 4")
+
+console.log(mesmoTipoNivel4([NaN, NaN]))
+console.log(mesmoTipoNivel4([NaN, 1]))
+
+// Nível 5
+
+function getTipoNivel5(valor) {
+    if (valor === null) {
+        return "null"
+    }
+
+    if (Number.isNaN(valor)) {
+        return "nan"
+    }
+
+    if (Array.isArray(valor)) {
+        return "array"
+    }
+
+    if (valor === undefined) {
+        return "undefined"
+    }
+
+    return typeof valor
+}
+
+function mesmoTipoNivel5(array) {
+    if (!Array.isArray(array)) {
+        return "Erro: Valor inválido!"
+    }
+
+    if (array.length === 0) {
+        return "Erro: Valor inválido!"
+    }
+
+    let posUm = getTipoNivel5(array[0])
+
+    for (let i = 1; i < array.length; i++) {
+        if (getTipoNivel5(array[i]) !== posUm) {
+            return false
+        }
+    }
+
+    return true
+}
+
+printTitulo("NÍVEL 5")
+
+console.log(mesmoTipoNivel5([[], []]))
+console.log(mesmoTipoNivel5([[], {}]))
+console.log(mesmoTipoNivel5([{}, {}]))
+
+console.log(mesmoTipoNivel5([[], null]))
+console.log(mesmoTipoNivel5([[], NaN]))
+console.log(mesmoTipoNivel5([{}, null]))
+
+console.log(mesmoTipoNivel5([undefined, undefined]))
+console.log(mesmoTipoNivel5([undefined, null]))
+
+// Nível 6
+
+function getTipoNivel6(valor) {
+    if (valor === null) {
+        return "null"
+    }
+
+    if (Number.isNaN(valor)) {
+        return "nan"
+    }
+
+    if (Array.isArray(valor)) {
+        return "array"
+    }
+
+    if (valor === undefined) {
+        return "undefined"
+    }
+
+    if (valor instanceof Date) {
+        return "date"
+    }
+
+    if (valor instanceof RegExp) {
+        return "regexp"
+    }
+
+    return typeof valor
+}
+
+function mesmoTipoNivel6(array) {
+    if (!Array.isArray(array)) {
+        return "Erro: Valor inválido!"
+    }
+
+    if (array.length === 0) {
+        return "Erro: Valor inválido!"
+    }
+
+    let posUm = getTipoNivel6(array[0])
+
+    for (let i = 1; i < array.length; i++) {
+        if (getTipoNivel6(array[i]) !== posUm) {
+            return false
+        }
+    }
+
+    return true
+}
+
+printTitulo("NÍVEL 6")
+
+console.log(mesmoTipoNivel6([new Date(), new Date()]))
+console.log(mesmoTipoNivel6([new Date(), {}]))
+
+console.log(mesmoTipoNivel6([/a/, /b/]))
+console.log(mesmoTipoNivel6([/a/, {}]))
+
+console.log(mesmoTipoNivel6([() => {}, function() {}]))
