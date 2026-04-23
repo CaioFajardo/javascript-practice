1a166bea511123acd18b2db3242a1501a47329dd
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-04-23 17:28:18 -0300

    refactor: changed function names

--- module-01-types/02-extra-exercises/exercise-07.js
+++ module-01-types/02-extra-exercises/exercise-07.js
@@ -6,38 +6,38 @@ function printTitulo(titulo) {
 
 // Nível 1
 
-function pegarNomeNível1(usuario) {
+function pegarNomeNivel1(usuario) {
     return usuario?.nome ?? "Nome não encontrado!"
 }
 
 printTitulo("NÍVEL 1")
 
-console.log(pegarNomeNível1({ nome: "Caio" }))
-console.log(pegarNomeNível1({}))
-console.log(pegarNomeNível1(null))
+console.log(pegarNomeNivel1({ nome: "Caio" }))
+console.log(pegarNomeNivel1({}))
+console.log(pegarNomeNivel1(null))
 
 // Nível 2
 
-function pegarCidadeNível2(usuario) {
+function pegarCidadeNivel2(usuario) {
     return usuario?.endereco?.cidade ?? "Cidade não encontrada!"
 }
 
 printTitulo("NÍVEL 2")
 
-console.log(pegarCidadeNível2({
+console.log(pegarCidadeNivel2({
     nome: "Caio",
     endereco: {
         cidade: "Rio de Janeiro"
     }
 }))
-console.log(pegarCidadeNível2({}))
-console.log(pegarCidadeNível2({ endereco: null }))
-console.log(pegarCidadeNível2({ endereco: {} }))
-console.log(pegarCidadeNível2(null))
+console.log(pegarCidadeNivel2({}))
+console.log(pegarCidadeNivel2({ endereco: null }))
+console.log(pegarCidadeNivel2({ endereco: {} }))
+console.log(pegarCidadeNivel2(null))
 
 // Nível 3
 
-function pegarCidadeNível3(usuario) {
+function pegarCidadeNivel3(usuario) {
     const cidade = usuario?.endereco?.cidade
 
     if (cidade === "") {
@@ -49,4 +49,4 @@ function pegarCidadeNível3(usuario) {
 
 printTitulo("NÍVEL 3")
 
-console.log(pegarCidadeNível3(""))
+console.log(pegarCidadeNivel3(""))
