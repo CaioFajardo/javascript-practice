68ce1f6f376c460d65963164477874882c8855d7
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-09 19:59:41 -0300

    feat: add exercise-08 (level 1)

---
+++ module-02-objects-arrays/02-extra-exercises/exercise-08.js
@@ -0,0 +1,21 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+const pessoaNivel1 = {}
+
+pessoaNivel1["nome"] = "Caio"
+
+console.log(pessoaNivel1)
+
+pessoaNivel1["nome"] = "Leandro"
+
+console.log(pessoaNivel1)
+
+delete pessoaNivel1["nome"]
+
+console.log(pessoaNivel1)
