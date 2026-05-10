04614de3697c7ad0a6cc6616138b43f5ed483fe6
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-10 01:26:58 -0300

    feat: add exercise-13 (level 2)

--- module-02-objects-arrays/02-extra-exercises/exercise-13.js
+++ module-02-objects-arrays/02-extra-exercises/exercise-13.js
@@ -31,5 +31,52 @@ for (const pessoa of arrPessoasNivel1) {
     todosNomesNivel1.push(pessoa.nome)
 }
 
+printTitulo("NÍVEL 1")
+
 console.log(propNivel1)
 console.log(todosNomesNivel1)
+
+// Nível 2
+
+const arrPessoasNivel2 = [
+    {
+        nome: "Caio",
+        idade: 22,
+        profissao: "Desenvolvedor",
+        cidade: "Cataguases",
+        ativo: true
+    },
+
+    {
+        nome: "Marina",
+        idade: 27,
+        profissao: "Designer",
+        cidade: "Juiz de Fora",
+        ativo: false
+    },
+
+    {
+        nome: "Lucas",
+        idade: 31,
+        profissao: "Analista de Infraestrutura",
+        cidade: "Belo Horizonte",
+        ativo: true
+    }
+]
+
+const pessoaCompletaNivel2 = []
+
+const pessoaPropsNivel2 = []
+
+for (const pessoa of arrPessoasNivel2) {
+    pessoaCompletaNivel2.push(pessoa)
+    
+    for (const [chave, valor] of Object.entries(pessoa)) {
+        pessoaPropsNivel2.push(`${chave}: ${valor}`)
+    }
+}
+
+printTitulo("NÍVEL 2")
+
+console.log(pessoaCompletaNivel2)
+console.log(pessoaPropsNivel2)
