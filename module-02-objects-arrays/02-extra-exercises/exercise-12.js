c532cbadb947e7951ec3f2cb45fe16f9466d1b8c
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-10 00:18:46 -0300

    feat: add exercise-12 (level 1)

---
+++ module-02-objects-arrays/02-extra-exercises/exercise-12.js
@@ -0,0 +1,19 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+const objNivel1 = {
+    arr: [
+        "azul",
+        "bege",
+        "branco"
+    ]
+}
+
+const acessArr = objNivel1.arr[0]
+
+console.log(acessArr)
