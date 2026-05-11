e84eda7d1cd45f38240507e157ef70652b498903
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-11 16:52:29 -0300

    feat: add exercise-05 (level 1)

---
+++ 02-extra-modules/01-logic-and-control-flow/exercise-05.js
@@ -0,0 +1,44 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+const valor = 5
+
+printTitulo("NÍVEL 1")
+
+switch (valor) {
+    case 1:
+        console.log("Domingo")
+        break
+
+    case 2:
+        console.log("Segunda-feira")
+        break
+
+    case 3:
+        console.log("Terça-feira")
+        break
+
+    case 4:
+        console.log("Quarta-feira")
+        break
+
+    case 5:
+        console.log("Quinta-feira")
+        break
+
+    case 6:
+        console.log("Sexta-feira")
+        break
+
+    case 7:
+        console.log("Sábado")
+        break
+
+    default:
+        console.log("Valor inválido!")
+}
