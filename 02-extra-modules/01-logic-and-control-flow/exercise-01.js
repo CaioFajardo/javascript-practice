306850a020aa197ae8f3aa990cc8b823c126702d
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-05-11 13:55:30 -0300

    feat: add extra modules and exercise-01

---
+++ 02-extra-modules/01-logic-and-control-flow/exercise-01.js
@@ -0,0 +1,20 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+const n1 = 3
+const n2 = 7
+const n2String = "7"
+
+printTitulo("NÍVEL 1")
+
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 >= n2)
+console.log(n1 <= n2)
+console.log(n2 == n2)
+console.log(n2 === n2String)
