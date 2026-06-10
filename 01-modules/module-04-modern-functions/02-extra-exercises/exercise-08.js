7322e9794a7f72e469d0b2b7d3f92d7edc4a5360
Caio Fajardo <caiocarvalhofajardo@gmail.com>
2026-06-10 05:45:37 -0300

    feat: add exercise 08 (level 1)

---
+++ 01-modules/module-04-modern-functions/02-extra-exercises/exercise-08.js
@@ -0,0 +1,15 @@
+function printTitulo(titulo) {
+    console.log("\n==============================")
+    console.log(titulo)
+    console.log("==============================")
+}
+
+// Nível 1
+
+printTitulo("NÍVEL 1")
+
+const soma = a => b => c => {
+    return a + b + c
+}
+
+console.log(soma(1)(2)(3))
